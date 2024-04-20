7a8cb18756a1eb520327007e17509edfe3d5bcf0
mritunjay.tiwari <mritunjay.tiwari@eagleview.com>
2024-04-20 10:59:00 +0530

    Added guest management endpoints

---
+++ app/models/Guest.js
@@ -0,0 +1,24 @@
+export default (sequelize, Sequelize) => {
+  const Guest = sequelize.define('guests', {
+    guestId: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      
+    },
+    contact: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    address: {
+      type: Sequelize.STRING
+    }
+  });
+
+  return Guest;
+};
